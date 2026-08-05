5d64a466d81a1c15b5b4c1405773a15d9996e9a1
MAMV1999 <mmanriquevarillas25@gmail.com>
2026-08-05 16:08:06 -0500

    05082026_1

--- BD/Personalizado.sql
+++ BD/Personalizado.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-07-2026 a las 14:49:00
+-- Tiempo de generación: 05-08-2026 a las 23:06:44
 -- Versión del servidor: 10.1.31-MariaDB
 -- Versión de PHP: 7.2.3
 
